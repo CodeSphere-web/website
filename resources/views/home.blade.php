59cee6860c34923a7861960d46068651d5067c2f
codesphere-web <codesphere27@gmail.com>
2026-07-17 21:52:27 +0300

    new version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" class="scroll-smooth">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="./codespherelogo.png" type="image/x-icon">   
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeSphere | Next-Gen Web Development in Lebanon</title>
     <!-- Tailwind CSS via Vite -->
